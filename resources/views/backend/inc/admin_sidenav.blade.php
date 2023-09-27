3cf47567e7d814132489ab40b7d4c69adcf55252
Rushil Sharma <srushil64@gmail.com>
2023-09-27 17:01:11 +0530

    -> label change Confirm -> Confirmed Orders

--- resources/views/backend/inc/admin_sidenav.blade.php
+++ resources/views/backend/inc/admin_sidenav.blade.php
@@ -254,7 +254,7 @@
                             <li class="aiz-side-nav-item">
                                 <a href="{{ route('all_orders.index') }}"
                                     class="aiz-side-nav-link {{ areActiveRoutes(['all_orders.index', 'all_orders.show']) }}">
-                                    <span class="aiz-side-nav-text">{{ translate('Confirm Orders') }}</span>
+                                    <span class="aiz-side-nav-text">{{ translate('Confirmed Orders') }}</span>
                                 </a>
                             </li>
                             <li class="aiz-side-nav-item">
